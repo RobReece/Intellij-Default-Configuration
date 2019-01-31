378f3762302a1d2664da77c7a86b59580331a184
RobReece <rob.reece@pega.com>
2019-01-31 09:17:38 -0500

    Initial repository creation

---
+++ A/src/api/java/SomeClassInProjectA_API.java
@@ -0,0 +1,3 @@
+public class SomeClassInProjectA_API {
+
+}
---
+++ B/src/main/java/Test1.java
@@ -0,0 +1,26 @@
+/*
+ * $
+ *
+ * Copyright (c) 2018  Pegasystems Inc.
+ * All rights reserved.
+ *
+ * This  software  has  been  provided pursuant  to  a  License
+ * Agreement  containing  restrictions on  its  use.   The  software
+ * contains  valuable  trade secrets and proprietary information  of
+ * Pegasystems Inc and is protected by  federal   copyright law.  It
+ * may  not be copied,  modified,  translated or distributed in  any
+ * form or medium,  disclosed to third parties or used in any manner
+ * not provided for in  said  License Agreement except with  written
+ * authorization from Pegasystems Inc.
+ */
+
+
+/**
+ * @author piotr
+ * @version $ 11/8/18
+ */
+public class Test1 {
+    public static final String COPYRIGHT = "Copyright (c) 2018  Pegasystems Inc.";
+    public SomeClassInProjectA a;
+    public SomeClassInProjectA_API apiA;
+}
